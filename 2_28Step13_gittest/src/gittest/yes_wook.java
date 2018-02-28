70a989c2e22da1c244b76d2014c0e6c94bdab46e
yes_wook <wjddnr0403@gmail.com>
2018-02-28 13:50:29 +0900

    "3"yeswook

---
+++ 2_28Step13_gittest/src/gittest/yes_wook.java
@@ -0,0 +1,10 @@
+package gittest;
+
+class yes_wook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm yes wook. JipAe GaGoSipDa");
+	}
+
+}
